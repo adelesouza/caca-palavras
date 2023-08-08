2a1bdd2de84cb1bac642589d5105adde76c5ed0c
Adele <adele.souza@hotmail.com>
2023-08-08 14:27:42 -0300

    quadro de palavras_parte1

--- src/main.js
+++ src/main.js
@@ -3,19 +3,51 @@ import { createHtmlTable } from './views.js'
 
 document.getElementById('btn').addEventListener('click', revealTable)
 
+const words = [
+  'palheta',
+  'uva',
+  'musica',
+  'palco',
+  'rua',
+  'casa',
+  'pincel',
+  'carro',
+  'caixa',
+  'foto',
+  'flor',
+  'luz'
+]
+
 function revealTable() {
   const numRows = document.getElementById('num_rows').value
   const numCols = document.getElementById('num_cols').value
-  const dataTable = createDataTable(numRows, numCols)
+  const wordsQuantity = calculateWordsQuantity(numRows)
+  const wordsToPlace = []
+  const availableWordsList = [...words]
+  while (wordsToPlace.length < wordsQuantity) {
+    let word = getRandomWord(availableWordsList)
+    wordsToPlace.push(word)
+  }
+  const dataTable = createDataTable(numRows, numCols, wordsToPlace)
   const table = createHtmlTable(dataTable)
-  adjustFrontElements(numRows, numCols)
+  adjustFrontElements(numRows, numCols, wordsToPlace)
   appendTableToContainer(table)
+  console.log('teste')
 }
 
-function adjustFrontElements(numRows, numCols) {
+function adjustFrontElements(numRows, numCols, wordsToPlace) {
   document.querySelector('.caca-palavras').style.display = 'flex'
   document.getElementById('numbers').innerHTML =
     '<h2 id="L">' + numRows + '</h2><h2>x</h2><h2 id="C">' + numCols + '</h2>'
+
+  for (let i = 0; i < wordsToPlace.length; i++) {
+    const words = document.getElementById('words')
+    const word = document.createTextNode(wordsToPlace[i] + ' - ')
+    while (words.firstChild) {
+      words.removeChild(words.firstChild)
+    }
+    words.appendChild(word)
+  }
 }
 
 function appendTableToContainer(table) {
@@ -24,4 +56,13 @@ function appendTableToContainer(table) {
     wordSearchContainer.removeChild(wordSearchContainer.firstChild)
   }
   wordSearchContainer.appendChild(table)
+}
+
+function getRandomWord(availableWordsList) {
+  const randomWordIndex = Math.floor(Math.random() * availableWordsList.length)
+  return availableWordsList.splice(randomWordIndex, 1)[0]
+}
+
+export function calculateWordsQuantity(numRows, rowsPerWord = 3) {
+  return Math.floor(numRows / rowsPerWord)
 }
--- src/models.js
+++ src/models.js
@@ -1,19 +1,4 @@
-const words = [
-  'palheta',
-  'uva',
-  'musica',
-  'palco',
-  'rua',
-  'casa',
-  'pincel',
-  'carro',
-  'caixa',
-  'foto',
-  'flor',
-  'luz'
-]
-
-export function createDataTable(numRows, numCols) {
+export function createDataTable(numRows, numCols, wordsToPlace) {
   let tableWithoutWords = []
   for (let i = 0; i < numRows; i++) {
     const row = []
@@ -23,7 +8,11 @@ export function createDataTable(numRows, numCols) {
     tableWithoutWords.push(row)
   }
   const wordsQuantity = calculateWordsQuantity(numRows, 3)
-  const tableWithWords = placeWords(wordsQuantity, tableWithoutWords)
+  const tableWithWords = placeWords(
+    wordsToPlace,
+    wordsQuantity,
+    tableWithoutWords
+  )
   const dataTable = fillTable(tableWithWords)
   return dataTable
 }
@@ -36,21 +25,22 @@ export function calculateWordsQuantity(numRows, rowsPerWord = 3) {
   return Math.floor(numRows / rowsPerWord)
 }
 
-export function placeWords(wordsQuantity, tableToPlaceWords) {
+export function placeWords(wordsToPlace, wordsQuantity, tableToPlaceWords) {
   let numRows = tableToPlaceWords.length
   let numCols = tableToPlaceWords[0].length
-  const availableWordsList = [...words]
+  //const availableWordsList = [...words]
   const rowsWithWords = []
   let wordsPlaced = 0
+  let j = 0
   while (wordsPlaced < wordsQuantity) {
     const rowToPlaceWord = getRandomRowToPlaceWord(numRows)
     if (!rowsWithWords.includes(rowToPlaceWord)) {
-      let word = getRandomWord(availableWordsList)
+      let word = wordsToPlace[j] //getRandomWord(availableWordsList)
+      j++
       let wordSplited = word.split('')
       let firstCharIndex = generateFirstCharIndex(wordSplited, numCols)
       let lastCharIndex = firstCharIndex + wordSplited.length
       let wordIndexCounter = 0
-
       for (let i = firstCharIndex; i < lastCharIndex; i++) {
         tableToPlaceWords[rowToPlaceWord][i] = wordSplited[wordIndexCounter]
         wordIndexCounter++
@@ -70,11 +60,6 @@ export function generateFirstCharIndex(word, numCols) {
 
 export function getRandomRowToPlaceWord(numRows) {
   return Math.floor(Math.random() * numRows)
-}
-
-export function getRandomWord(availableWordsList) {
-  const randomWordIndex = Math.floor(Math.random() * availableWordsList.length)
-  return availableWordsList.splice(randomWordIndex, 1)[0]
 }
 
 export function fillTable(tableWithWords) {
